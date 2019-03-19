e0661f06abbbb9bb5d769723cf5b3b1a6198419b
Anisha-goyal <33254842+Anisha-goyal@users.noreply.github.com>
2019-03-19 15:35:42 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+	<title>Student Management System</title>
+	<link href="../css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+	<title>Student Management System</title>
+</head>
+<!--<body bgcolor="#f2f2f2">-->
+<body>
+	<br>
+<style>
+body{
+	background-image:url('images/background.jpg');
+	background-repeat: no-repeat;
+	background-size:cover;
+	margin-top:-150px;
+	line-height:140px;
+
+}
+#title{
+	font-size:60px;
+	background-color:#000000;
+	margin:auto;
+	border-radius:5px;
+	color:#ffffff;
+}
+#admin{
+	margin-top:-80px;
+	font-size:40px;
+}
+#student{
+	margin-top:-200px;
+	font-size:40px;
+}
+
+a{
+	text-decoration: none;
+	border-radius:4px;
+	color: #0d0d0d;
+	font-weight: bold;
+
+}
+a:hover{
+    color: #333333;
+}
+
+</style>	
+<h1 align="center" id="title">Welcome to Student Management System</h1><br>
+<h3 align="center" id="admin"><a href="admin/adminlogin.php">ADMIN LOGIN</a></h3><br>
+<h3 align="center" id="student"><a href="student/studentlogin.php">STUDENT LOGIN</a></h3>
+</body>
+</html>
